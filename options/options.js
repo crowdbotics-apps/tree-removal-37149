7ef7b784cb5e6ebb680a1b1eb44dd679d3ac574c
Crowdbotics <team@crowdbotics.com>
2022-09-26 06:52:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tree_removal_37149",
+  url: "https://tree_removal_37149.botics.co",
+  api: "https://tree_removal_37149.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
